6f60d22579dcf68bca60f60b24c0f99b5e1c85a8
Danilo <petkovicdanilo97@gmail.com>
2021-09-09 23:45:16 +0200

    Add pick registry options

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 mod commands;
 
+use std::str::FromStr;
+
 use anyhow::Result;
 use clap::Clap;
 use commands::{config, index, manifest, pull, pull_layer, tags};
-use oci_registry::registry::{Registry, RegistryType};
+use oci_registry::registry::Registry;
+
+#[derive(Clap, Debug)]
+enum KnownRegistry {
+    Docker,
+    Quay,
+    Mcr,
+}
+
+impl FromStr for KnownRegistry {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "docker" => Ok(KnownRegistry::Docker),
+            "quay" => Ok(KnownRegistry::Quay),
+            "mcr" => Ok(KnownRegistry::Mcr),
+            _ => Err("no match"),
+        }
+    }
+}
 
 /// Container Registry Client -
 /// command line utility to communicate with OCI registries
 #[derive(Clap, Debug)]
 #[clap(name = "crec")]
 struct Opt {
-    /// Registry to work with
+    /// Registry to work with. Possible values:
+    /// docker, quay, mcr
     #[clap(long, default_value = "docker")]
-    registry: String,
+    registry: KnownRegistry,
+
+    /// Url of the registry without https:// in front.
+    /// For example: registry-1.docker.io
+    #[clap(long, conflicts_with = "registry")]
+    registry_url: Option<String>,
 
     #[clap(subcommand)]
     subcommand: SubCommand,
@@ -28,10 +56,22 @@ enum SubCommand {
     Pull(pull::Pull),
 }
 
+fn known_registry_url(registry: &KnownRegistry) -> String {
+    match registry {
+        KnownRegistry::Docker => String::from("https://registry-1.docker.io"),
+        KnownRegistry::Quay => String::from("https://quay.io"),
+        KnownRegistry::Mcr => String::from("https://mcr.microsoft.com"),
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let opt = Opt::parse();
-    let registry = Registry::new(RegistryType::Docker);
+    let registry_url = match opt.registry_url {
+        Some(registry_url) => format!("https://{}", registry_url),
+        None => known_registry_url(&opt.registry),
+    };
+    let registry = Registry::new(&registry_url);
 
     match opt.subcommand {
         SubCommand::Manifest(manifest) => manifest.exec(registry).await?,
